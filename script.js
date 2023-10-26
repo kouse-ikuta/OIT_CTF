cdba44110c35422714c9e4735d313a7ecc934915
kouse-ikuta <e1n21009@oit.ac.jp>
2023-10-26 17:13:49 +0900

    update q0

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var score = 0;
 var scorel=0;
 counter=0;
 
+function hin0(){
+    document.getElementById("url1").textContent = "アルファベットの順番に注目";
+}
 function hint1(){
     document.getElementById("url1").textContent = "";
     document.getElementById("url2").textContent = "";
